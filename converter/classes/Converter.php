67afc4fb80ea8333ba38293bda3c7a9adb62f4e9
Candice Canoso <mail@cndce.me>
2021-04-24 23:23:16 +0800

    Changed class `Converter` call to `self`; Renamed function `convert_xml` to `aws_to_jw_data`

--- converter/classes/Converter.php
+++ converter/classes/Converter.php
@@ -13,17 +13,17 @@ class Converter{
          * Do not do anything if $VIDEO_MAP has already
          * been initializied
          */
-        if(!is_null(Converter::$VIDEO_MAP))
+        if(!is_null(self::$VIDEO_MAP))
             return;
 
         if(!($file = fopen(VIDEO_MAP_URL, 'r'))){
             die('Invalid Video Map URL');
         }
 
-        Converter::$VIDEO_MAP = [];
+        self::$VIDEO_MAP = [];
 
         while($line = fgetcsv($file, 0, "\t")){
-            Converter::$VIDEO_MAP[$line[0]] = [
+            self::$VIDEO_MAP[$line[0]] = [
                 'jw'    => $line[1],
                 'test'  => $line[3]
             ];
@@ -33,19 +33,19 @@ class Converter{
     
 
     public static function aws_to_jw_url(string $awsUrl){
-        Converter::__init_video_map();
+        self::__init_video_map();
 
         [, $filename] = explode(AWS_BASE_URL, $awsUrl);
 
 
-        if(!isset(Converter::$VIDEO_MAP[$filename]['jw'])){
+        if(!isset(self::$VIDEO_MAP[$filename]['jw'])){
             return $awsUrl;
         }
 
-        return JW_BASE_URL . Converter::$VIDEO_MAP[$filename]['jw'] . '.m3u8';
+        return JW_BASE_URL . self::$VIDEO_MAP[$filename]['jw'] . '.m3u8';
     }
 
-    public static function convert_xml($playlistUrl){
+    public static function aws_to_jw_data(string $playlistUrl){
 
         if(!file_exists($playlistUrl)){
             echo $playlistUrl . ' - ';
@@ -60,7 +60,7 @@ class Converter{
             $attributes = $playlistItem->children('media', true)->attributes();
 
             $item = [
-                'file'      => Converter::aws_to_jw_url($attributes['url'])
+                'file'      => self::aws_to_jw_url($attributes['url'])
             ];
 
             if(!empty($attributes['start'])){
