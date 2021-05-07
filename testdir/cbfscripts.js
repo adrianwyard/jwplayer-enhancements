b90bcf16eb9648dc00bd9b569ef0e4b105816ca1
Adrian Wyard <42127412+adrianwyard@users.noreply.github.com>
2021-05-06 22:37:21 -0700

    Refined params and added other function variants

--- testdir/cbfscripts.js
+++ testdir/cbfscripts.js
@@ -1,43 +1,85 @@
-	function mediaWindowHtml(ctf) {
-		var timeout=0;
+function mediaWindowHtml(ctf) {
 
-		document.write('<center><div id="myElement">Loading the player...</div></center>');
+	document.write('<center><div id="myElement">Loading the player...</div></center>');
 
+	var ctfarr = ctf.split("/");
 
-		// This timeout on small screens was a hack to stop a load sequencing error. May not need this anymore.
-		if(window.innerWidth<=600){ 
-			timeout=3000;
-		}
+	new CBPlayer({
+		ctf: 'https://counterbalance.net/converter/index.php?playlist=' + ctfarr[1] + "/" + ctfarr[2] + CBPlayer.PLAYLIST_RSS_EXTENSION,
+		el: 'myElement',
+		autostart: 'false',
+		repeat: false,
+		height: 330,
+		width: 440
+	})
 
+}
 
+function mediaWindowHtml2(ctf) {
 
-		setTimeout(function () {
-			var ctfarr = ctf.split("/");
+	document.write('<center><div id="myElement2">Loading the player...</div></center>');
 
-			new CBPlayer({
-				ctf: CBPlayer.PLAYLIST_BASE_URL + ctfarr[1] + "/" + ctfarr[2] + CBPlayer.PLAYLIST_RSS_EXTENSION,
-				el: 'myElement',
-				allowscriptaccess: "always",
-				// autostart: "true",
-				allowfullscreen: "true",
-				// repeat: "list",
-				height: 264,
-				width: 352,
-			})
+	var ctfarr = ctf.split("/");
 
+	new CBPlayer({
+		ctf: 'https://counterbalance.net/converter/index.php?playlist=' + ctfarr[1] + "/" + ctfarr[2] + CBPlayer.PLAYLIST_RSS_EXTENSION,
+		el: 'myElement2',
+		autostart: 'false',
+		repeat: false,
+		height: 330,
+		width: 440
+	})
 
-		}, timeout);
-	}
-	
+}
 
-	/* Also need the following near duplicate functions that are called from existing pages:
+function mediaWindowHtml3(ctf) {
 
-	function mediaWindowHtml2(ctf) with <div id="container2"> to allow for multiple videos on a single page.
+	document.write('<center><div id="myElement3">Loading the player...</div></center>');
 
-	function mediaWindowHtml3(ctf) with <div id="container3">
+	var ctfarr = ctf.split("/");
 
-	function mediaWindowHtmlmin(ctf) with <div id="containermin">
+	new CBPlayer({
+		ctf: 'https://counterbalance.net/converter/index.php?playlist=' + ctfarr[1] + "/" + ctfarr[2] + CBPlayer.PLAYLIST_RSS_EXTENSION,
+		el: 'myElement3',
+		autostart: 'false',
+		repeat: false,
+		height: 340,
+		width: 720
+	})
 
-	function audioWindowHtml(ctf) with just height for seek bar.
+}
 
-	*/
+function mediaWindowHtmlmin(ctf) {
+
+	document.write('<center><div id="myElementmin">Loading the player...</div></center>');
+
+	var ctfarr = ctf.split("/");
+
+	new CBPlayer({
+		ctf: 'https://counterbalance.net/converter/index.php?playlist=' + ctfarr[1] + "/" + ctfarr[2] + CBPlayer.PLAYLIST_RSS_EXTENSION,
+		el: 'myElementmin',
+		autostart: 'false',
+		repeat: false,
+		height: 330,
+		width: 440
+	})
+
+}
+
+function audioWindowHtml(ctf) {
+
+	document.write('<center><div id="myElement">Loading the player...</div></center>');
+
+	var ctfarr = ctf.split("/");
+
+	new CBPlayer({
+		ctf: 'https://counterbalance.net/converter/index.php?playlist=' + ctfarr[1] + "/" + ctfarr[2] + ".xml",
+		el: 'myElement',
+		autostart: 'false',
+		repeat: false,
+		height: 100,
+		width: 440
+	})
+
+}
+
